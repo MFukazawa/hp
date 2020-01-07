4de75f682e61a71ae744c082264a8a9786d14ac2
Micah Fukazawa <micahfukazawa@gmail.com>
2020-01-07 11:17:47 +0900

    first commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="layout">
+    <nav class="nav">
+      <div class="container">
+        <div class="nav__logo">
+          <g-link class="nav__logo-link" to="/">
+            <img class="nav__img" src="../assets/logo.jpg">
+          </g-link>
+        </div>
+        <div class="nav__list">
+          <g-link class="nav__list-item" to="/about">About</g-link>
+          <g-link class="nav__list-item" to="/work/">Work</g-link>
+          <g-link class="nav__list-item" to="/contact/">Contact</g-link>
+        </div>
+      </div>
+    </nav>
+    <slot/>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+  body {
+    font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+    margin:0;
+    padding:0;
+    line-height: 1;
+    font-size: 1.6rem
+  }
+
+  a {
+    background-color: transparent;
+    text-decoration: none;
+    color: inherit;
+    outline: none;
+      -webkit-tap-highlight-color: transparent;
+  }
+
+  .layout {
+    margin: 0 auto;
+    color: #5d5d5d;
+  }
+
+  .nav {
+    position: relative;
+    padding: 1em 0;
+    align-items: center;
+    z-index: 10;
+  }
+
+  .container {
+    display: flex;
+    flex-flow: row wrap;
+    width: 100%;
+    align-items: center;
+    max-width: 1400px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .nav__logo {
+    flex: 1 1 0;
+    align-self: flex-start;
+    height: 120px;
+  }
+
+  .nav__logo-link {
+    margin-top: 10px;
+    display: flex;
+    align-content: center;
+    align-items: center;
+  }
+
+  .nav__list {
+    flex: 1 1 0;
+    margin: 0;
+    padding: 0;
+    text-align: right;
+    height: 120px;
+  }
+
+  .nav__list-item {
+    text-decoration: none;
+    display: inline-block;
+    padding: .5em 1em;
+    letter-spacing: .5px;
+    font-size: .8em;
+    transition: color .25s ease;
+  }
+
+  @media screen and (max-width: 360px) {
+    .nav__logo img {
+      max-width: 150px;
+    }
+  }
+
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,86 @@
+<template>
+  <Layout>
+    <section class="hero">
+      <div class="container">
+        <h1 class="hero__title">
+          I'm Micah Fukazawa, a
+          <a href="https://github.com/MFukazawa/" target="_blank" class="hero__link">
+            front-end developer
+          </a> 
+          living in Miyazaki, Japan, currently working for
+          <a href="https://nomadori.com/" target="_blank">Nomadori</a>
+          .
+        </h1>
+      </div>
+      <div class="container">
+        <div class="hero__employer">
+          <h3 class="employer__label">Currently:</h3>
+          <a target="_blank" href="https://nomadori.com/">
+            <g-image class="hero__employer-img" src="~/assets/nomadori_logo.png"></g-image>
+          </a>
+        </div>
+      </div>
+
+    </section>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+    </p>
+
+    <div class="logo">
+      MF
+    </div>
+
+    <p class="home-links">
+      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
+      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+    </p>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style scoped>
+  .hero__title {
+    position: relative;
+    max-width: 840px;
+    margin-top: .5em;
+    margin-bottom: 1.5em;
+    letter-spacing: -1px;
+    line-height: 1.8;
+    font-size: 2rem;
+    font-weight: 400;
+  }
+
+  .hero__link {
+    color: cornflowerblue;
+    transition: color .25s ease;
+  }
+
+  .hero__link:hover {
+    color: #5d5d5d;
+  }
+
+  .hero__employer-img {
+    margin-left: 5px;
+  }
+
+  .employer__label {
+    margin-bottom: 1em;
+    letter-spacing: 2px;
+    text-transform: uppercase;
+    font-size: 1.5rem;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+
+</style>
+
