b4635f5258f72970e90aec3ff1f1c1c79b1c0c8e
MFukazawa <micahfukazawa@gmail.com>
2020-10-26 17:54:09 +0900

    [Update] NPM Update, Add some about section information

--- src/components/sections/AboutSection.vue
+++ src/components/sections/AboutSection.vue
@@ -1,7 +1,23 @@
 <template>
   <section id="about" class="h-screen">
     <base-container>
-        abc
+      <h2 class="text-4xl font-bold my-8">About</h2>
+      <p>
+        Hi, I'm Micah.  I work remotely as a Frontend Developer at Nomadori in Miyazaki, Japan.
+        I was born and raised in the United States of America.
+        I've built
+        <a href="https://www.miyazakijazz.org">Miyazaki Jazz Organization</a> and 
+        <a href="https://www.nomadori.com">Nomadori</a>, 
+        along with a number of private projects.
+      </p>
+
+      <ul>
+        <label>Some of my favorite tools:</label>
+        <li class="list-disc">Nuxt.js</li>
+        <li class="list-disc">Vue.js</li>
+        <li class="list-disc">JavaScript</li>
+        <li class="list-disc">TailwindCSS</li>
+      </ul>
     </base-container>
   </section>
 </template>
@@ -18,6 +34,6 @@ export default {
 
 <style scoped>
   #about {
-    background-color: cornflowerblue
+    background-color: mediumseagreen;
   }
 </style>
