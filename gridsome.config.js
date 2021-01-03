062d477f1cc456bfb77762f68054bd03118148a3
MFukazawa <micahfukazawa@gmail.com>
2021-01-03 19:54:06 +0900

    comment out gridsome contentful plugin

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,21 +7,21 @@
 module.exports = {
   siteName: 'Micah Fukazawa | Developer',
   plugins: [
-    {
-      use: "@gridsome/source-contentful",
-      options: {
-        space: process.env.CONTENTFUL_SPACE,
-        accessToken: process.env.CONTENTFUL_TOKEN,
-        host: "cdn.contentful.com",
-        environment: process.env.CONTENTFUL_ENVIRONMENT,
-        typename: "Contentful"
-      }
-    },
+    // {
+    //   use: "@gridsome/source-contentful",
+    //   options: {
+    //     space: process.env.CONTENTFUL_SPACE,
+    //     accessToken: process.env.CONTENTFUL_TOKEN,
+    //     host: "cdn.contentful.com",
+    //     environment: process.env.CONTENTFUL_ENVIRONMENT,
+    //     typename: "Contentful"
+    //   }
+    // },
     {
       use: "gridsome-plugin-tailwindcss"
     }
   ],
-  templates: {
-    ContentfulBlogPost: '/blog/:slug',
-  }
+  // templates: {
+  //   ContentfulBlogPost: '/blog/:slug',
+  // }
 }
